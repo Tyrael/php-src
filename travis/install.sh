fa30477e89fdb6db7e22001717aabcc67fd77f3e
Ferenc Kovacs <tyrael@php.net>
2014-07-07 14:13:55 +0200

    libicu-dev doesn't support multiarch yet

--- travis/install.sh
+++ travis/install.sh
@@ -12,7 +12,8 @@ if [[ "$ENABLE_IA32" == 1 ]]; then
 	sudo aptitude install -y libxml2-dev:i386;
 	sudo mv /usr/bin/xml2-config /usr/bin/xml2-config.i386;
 	sudo cp /usr/bin/xml2-config.x86_64 /usr/bin/xml2-config;
-	sudo aptitude install -y zlib1g-dev:i386 libgmp-dev:i386 libicu-dev:i386 libmcrypt-dev:i386 libtidy-dev:i386;
+	# sudo aptitude install -y libicu-dev:i386 # not multiarch ready yet: https://bugs.launchpad.net/ubuntu/+source/icu/+bug/992439
+	sudo aptitude install -y zlib1g-dev:i386 libgmp-dev:i386 libmcrypt-dev:i386 libtidy-dev:i386;
 	export CFLAGS='-m32';
 	export CPPFLAGS='-m32';
 	export CCASFLAGS='-m32';
